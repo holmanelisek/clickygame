5cc47ed64486410bc6fd4267a00b55b9f2926247
EKaye <holmanelisek@gmail.com>
2020-01-06 13:25:51 -0500

    pseudo

---
+++ src/components/counter.js
@@ -0,0 +1,3 @@
+//on click
+//if clicked is true, set counter to 0, set all clicked to false, shuffle photos
+//if clicked is false, counter +1, set clicked to true
---
+++ src/components/photo.js
@@ -0,0 +1,5 @@
+//photo state clicked true or false (will pass to counter and photoboard)
+//on click
+    //set positioned to false
+    //if true, all set to false 
+    //if false, set to true 
